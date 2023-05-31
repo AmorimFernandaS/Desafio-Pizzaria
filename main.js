5a1039040f2992aeb248640ab196c75a828c27d9
Fernanda Amorim <fernanda.amorim@CLBBRC02ZXJV6MD6M.local>
2023-05-30 23:36:14 -0300

    segundo

---
+++ main.js
@@ -0,0 +1,13 @@
+function calcularDivisao() {
+    var valorConta = parseFloat(document.getElementById('valor-conta').value);
+    var valorTaxa = parseFloat(document.getElementById('valor-taxa').value);
+    var quantidadePessoas = parseInt(document.getElementById('quantidade-pessoas').value);
+  
+    if (isNaN(valorConta) || isNaN(valorTaxa) || isNaN(quantidadePessoas)) {
+      document.getElementById('resultado').innerHTML = 'Por favor, preencha todos os campos corretamente.';
+    } else {
+      var valorTotal = valorConta + valorTaxa;
+      var valorPorPessoa = valorTotal / quantidadePessoas;
+      document.getElementById('resultado').innerHTML = 'Cada pessoa deve pagar R$ ' + valorPorPessoa.toFixed(2);
+    }
+  }
